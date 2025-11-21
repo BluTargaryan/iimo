e05e625566887d24b7fb6b3106c191474adeb9cd
BluTargaryan <opeyemiyolo@gmail.com>
2025-11-21 17:44:00 +0100

    initial setup

---
+++ app/components/sections/TopHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopHeader = () => {
+  return (
+    <header className="fixed top-0 left-1/2 transform -translate-x-1/2 z-50 flex justify-center items-center w-full h-16 mx-auto">
+        <span className='font-semibold'>iimo</span>
+    </header>
+  )
+}
+
+export default TopHeader
