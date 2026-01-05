4573ed9c089afa7c4c09237c28e37aa8329a9276
BluTargaryan <opeyemiyolo@gmail.com>
2026-01-05 18:07:16 +0100

    checkpoint: edited header at mobile view for studio pages

--- app/components/sections/TopHeaderLoggedIn.tsx
+++ app/components/sections/TopHeaderLoggedIn.tsx
@@ -4,17 +4,23 @@ import logoutIcon from '@/app/assets/images/logout.svg'
 
 const TopHeader = () => {
   return (
-    <header className="fixed top-0 inset-x-4 z-50 flex justify-between  items-center h-16 border-b bg-background
+    <header className="fixed top-0 inset-x-4 z-50 flex flex-col justify-between py-3 gap-2  border-b bg-background
+    sm:items-center sm:h-16 sm:flex-row sm:py-0
     md:inset-x-10
     xl:inset-x-0 xl:max-w-[1144px] xl:mx-auto xl:h-17
     ">
         <span className='font-black xl:text-xl'>iimo</span>
 
-        <span className='row-flex font-normal gap-8'>
-            <span>Title</span>
-            <span className='row-flex gap-2 items-center'>
+        <span className='row-flex font-normal gap-3 text-sm'>
+            <span>Shoots</span>
+            <span>Clients</span>
+        </span>
+
+        <span className='row-flex font-normal gap-3 text-sm'>
+            <span>Notifications</span>
+            <span className='row-flex gap-1 items-center'>
             <span>Logout</span>
-            <Image src={logoutIcon} alt="logout" width={20} height={20} className='w-4 h-auto'/>
+            <Image src={logoutIcon} alt="logout" width={20} height={20} className='w-3 h-auto'/>
             </span>
         </span>
     </header>
